1ac8cbf7b4196844979c181bbb3b05d97e782d51
Sunhee Han <63505110+Han7sunny@users.noreply.github.com>
2022-03-03 19:32:42 +0900

    Create 점프와 순간 이동.java

---
+++ 알고리즘/프로그래머스/자바/점프와 순간 이동.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int solution(int n) {
+        int energy = 0;
+        while(n > 0) {
+          if(n % 2 != 0)
+            energy++;
+          n = n / 2;
+        }
+        return energy;
+    }
+}
