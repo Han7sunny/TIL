07e705b77a0c4e12617830b50caa398c177b81a2
Sunhee Han <63505110+Han7sunny@users.noreply.github.com>
2022-03-13 00:12:25 +0900

    Update 연산자 끼워넣기.java

--- 알고리즘/백준/연산자 끼워넣기.java
+++ 알고리즘/백준/연산자 끼워넣기.java
@@ -1,14 +1,8 @@
-package 프로그래머스;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -22,11 +16,11 @@ public class Main {
 				if(pick[i] == 0)
 					sum += pick[i+1];
 				else if(pick[i] == 1)
-					sum = sum - pick[i+1];
+					sum -= pick[i+1];
 				else if(pick[i] == 2)
-					sum = sum * pick[i+1];
+					sum *= pick[i+1];
 				else
-					sum = sum / pick[i+1];
+					sum /= pick[i+1];
 			}
 			min = Math.min(min, sum);
 			max = Math.max(max, sum);
@@ -34,14 +28,13 @@ public class Main {
 		}
 		int lastIndex = pick.length - 2*k;
 		for(int i = 0; i < op.length; i++) {
-			if(op[i] != 0) {
+			if(op[i] > 0) {
 				pick[lastIndex] = i;
 				op[i]--;
 				cal(pick,k-1);
 				op[i]++;
 			}
 		}
-		
 	}
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
@@ -50,7 +43,7 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		int[] num = new int[2*N-1];
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i <= N; i+=2) {
+		for(int i = 0; i < 2*N; i+=2) {
 			num[i] = Integer.parseInt(st.nextToken());
 		}
 		st = new StringTokenizer(br.readLine());
