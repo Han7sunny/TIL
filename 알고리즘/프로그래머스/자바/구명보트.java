ae5fed82326e453d4312fc96abba98199f65f3bb
Sunhee Han <63505110+Han7sunny@users.noreply.github.com>
2022-05-18 00:54:26 +0900

    Update 구명보트.java

--- 알고리즘/프로그래머스/자바/구명보트.java
+++ 알고리즘/프로그래머스/자바/구명보트.java
@@ -3,6 +3,8 @@ import java.util.Deque;
 import java.util.LinkedList;
 class Solution {
     public int solution(int[] people, int limit) {
+        
+        // 1. Deque 사용
         int answer = 0;
     	Deque<Integer> boat = new LinkedList<>();
     	Arrays.sort(people);
@@ -15,7 +17,6 @@ class Solution {
         if(boat.peekFirst() + boat.pollLast() <= limit) 
     		  boat.pollFirst();
         
-        
 //         if(boat.peekFirst() + boat.peekLast() > limit) { 
 //     			boat.pollLast();
 //     		}
@@ -27,5 +28,25 @@ class Solution {
     		answer++;
     	}
     	return answer;
+        
+        
+        // 2. 배열 사용
+        int answer = 0;
+        int start = 0;
+        int end = people.length - 1;
+    	Arrays.sort(people);
+    	
+    	while(start <= end) {
+            // 맨 앞과 맨 뒤를 뽑았는데 값 초과 -> 결국 모두 다 초과할 수 밖에 없으니까 뒤에 값 혼자 빼줌
+            if(people[start] + people[end] <= limit) 
+                start++;
+            end--;
+            answer++;
+           
+    	}
+    	return answer;
+
     }
 }
+
+
