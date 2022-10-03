6b959c7d1b8d0d37ef6dabf1a33b659a43d87c53
Han7sunny <xoa1235@gmail.com>
2022-10-03 23:47:50 +0900

    feat:Add BOJ_14889

--- 알고리즘/백준/Main_14889_스타트와링크.java
+++ 알고리즘/백준/Main_14889_스타트와링크.java
@@ -4,32 +4,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
+
 public class Main_14889_스타트와링크 {
+	static int N;
 	static int[][] team;
 	static int answer = Integer.MAX_VALUE;
 
-	public static int[] otherTeam(int[] team, int[] picked) {
-		boolean[] checkedTeam = new boolean[team.length];
-		int[] otherTeam = new int[picked.length];
+	public static int[] otherTeam(boolean[] picked) {
+		int[] otherTeam = new int[N/2];
 		int idx = 0;
-		for (int i = 0; i < team.length && idx < picked.length; i++) {
-			if (team[i] == picked[idx]) {
-				checkedTeam[i] = true;
-				idx++;
-			}
-		}
-
-		idx = 0;
-		for (int i = 0; i < team.length && idx < picked.length; i++) {
-			if (!checkedTeam[i]) {
-				otherTeam[idx++] = team[i];
-			}
+		for (int i = 0; i < N; i++) {
+			if(picked[i] == false)
+				otherTeam[idx++] = i;
 		}
 		return otherTeam;
 	}
 
 	public static int synergy(int[] teamComb, int[] picked, int k, int preIdx) {
-		// 2개
 		int sum = 0;
 		if (k == 2) {
 			return sum += team[picked[0]][picked[1]];
@@ -46,24 +37,25 @@ public class Main_14889_스타트와링크 {
 				}
 			}
 		}
-
 		return sum;
 	}
 
-	public static void teamComb(int[] myTeam, int[] picked, int k, int idx, int length) {
-		if (k == length) {
-			int teamA = synergy(picked, new int[2], 0, 0);
-			int teamB = synergy(otherTeam(myTeam, picked), new int[2], 0, 0);
+	public static void teamComb(int[] myTeam, boolean[] picked, int k, int idx) {
+		if (k == N/2) {
+			int teamA = synergy(myTeam, new int[2], 0, 0);
+			int teamB = synergy(otherTeam(picked), new int[2], 0, 0);
 			answer = Math.min(answer, Math.abs(teamA - teamB));
 			return;
 		}
-		int len = myTeam.length;
+        int len = N;
 		if (k == 0) {
 			len = 1;
 		}
 		for (int i = idx; i < len; i++) {
-			picked[k] = myTeam[i];
-			teamComb(myTeam, picked, k + 1, i + 1, length);
+			myTeam[k] = i;
+			picked[i] = true;
+			teamComb(myTeam, picked, k + 1, i + 1);
+			picked[i] = false;
 		}
 	}
 
@@ -73,21 +65,20 @@ public class Main_14889_스타트와링크 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 
-		int N = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 		team = new int[N][N];
-		int[] teamNum = new int[N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			teamNum[i] = i;
 			for (int j = 0; j < N; j++) {
 				team[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 
-		teamComb(teamNum, new int[N / 2], 0, 0, N / 2);
+		teamComb(new int[N/2], new boolean[N], 0, 0);
 		bw.write(Integer.toString(answer));
 		br.close();
 		bw.flush();
 		bw.close();
 	}
+
 }
