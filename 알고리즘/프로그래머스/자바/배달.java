bdf405807fa278083f42e1717d04fb5c4db48f21
Sunhee Han <63505110+Han7sunny@users.noreply.github.com>
2024-07-06 10:45:10 +0900

    [Feat]: Update 배달.java

--- 알고리즘/프로그래머스/자바/배달.java
+++ 알고리즘/프로그래머스/자바/배달.java
@@ -1,40 +1,35 @@
-import java.util.PriorityQueue;
-import java.util.ArrayList;
+import java.util.*;
 class Solution {
     public int solution(int N, int[][] road, int K) {
-        boolean[] visited = new boolean[N+1];
-        int[] time = new int[N+1];
-        ArrayList<int[]>[] route = new ArrayList[N+1];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[1]-b[1]);
-        int answer = N;
-        
-        for(int town = 1; town <= N; town++){
-            route[town] = new ArrayList<>();
-            time[town] = Integer.MAX_VALUE;
+        //  K 시간 이하로 배달이 가능한 마을에서만 주문을 받으려고 한다.
+        //  1번 마을 기준
+        int answer = 1;
+        int[][] dp = new int[N+1][N+1];
+        for(int i = 1; i <= N; i++){
+            Arrays.fill(dp[i], 500001);
         }
         
         for(int i = 0; i < road.length; i++){
-            route[road[i][0]].add(new int[]{road[i][1],road[i][2]});
-            route[road[i][1]].add(new int[]{road[i][0],road[i][2]});
+            int s = road[i][0];
+            int e = road[i][1];
+            int t = road[i][2];
+            if(dp[s][e] > t){
+                dp[s][e] = t;
+                dp[e][s] = t;
+            }
         }
         
-        visited[1] = true;
-        pq.add(new int[]{1,0});
-        while(!pq.isEmpty()){
-            int[] now = pq.poll();
-            if(!visited[now[0]]){
-                time[now[0]] = now[1];
-                if(time[now[0]] > K)
-                    answer--;
-            }
-            visited[now[0]] = true;
-            for(int i = 0; i < route[now[0]].size(); i++){
-                int[] next = route[now[0]].get(i);
-                if(time[next[0]] > next[1] + now[1]){
-                    time[next[0]] = next[1] + now[1];
-                    pq.add(new int[]{next[0], next[1] + now[1]});
+        for(int k = 1; k <= N; k++){
+            for(int i = 1; i <= N; i++){
+                for(int j = 1; j <= N; j++){
+                    dp[i][j] = Math.min(dp[i][k] + dp[k][j], dp[i][j]);                    
                 }
             }
+        }
+        
+        for(int i = 2; i <= N; i++){
+            if(dp[1][i] <= K)
+                answer++;
         }
         
         return answer;
